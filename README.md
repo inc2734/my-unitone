5ac44d2eee7932cfb27525c88532e7ff604015be
inc2734 <inc@2inc.org>
2024-05-10 09:10:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# My unitone
+
+子テーマの `functions.php` にカスタマイズコードを追加するように、このプラグインの `my-unitone.php` に書くと、同じようにカスタマイズが反映されます。
+
+## 定数
+
+下記の定数が利用可能です。
+
+### MY_UNITONE_URL
+
+My unitone プラグインディレクトリへの URL
+
+### MY_UNITONE_PATH
+
+My unitone プラグインディレクトリへの ファイルパス
+
+### プラグインが読み込まれてるか確認したい場合
+
+このプラグインが読み込まれてる場合は、ブラウザのコンソールに「My unitone plugin is active」というメッセージが表示されます。
---
+++ my-unitone.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin name: My unitone
+ * Description: このプラグインに、あなたの unitone 用カスタマイズコードを書いてください。
+ * Version: 0.1.0
+ * Update URI: https://unitone.2inc.org
+ *
+ * @package my-unitone
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+$theme = wp_get_theme( get_template() );
+if ( 'unitone' !== $theme->template ) {
+	return;
+}
+
+/**
+ * Directory url of this plugin
+ *
+ * @var string
+ */
+define( 'MY_UNITONE_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
+
+/**
+ * Directory path of this plugin
+ *
+ * @var string
+ */
+define( 'MY_UNITONE_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+
+/**
+ * Display message in console.log if this plugin is enabled.
+ */
+add_action(
+	'wp_footer',
+	function () {
+		if ( is_user_logged_in() ) :
+			?>
+			<script>console.log( 'My unitone plugin is active' );</script>
+			<?php
+		endif;
+	}
+);
